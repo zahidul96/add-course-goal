9deeddd11dc2d4ac35d90ee3b895af8e620725ad
zahidul96 <zahidulhussain111@gmail.com>
2026-02-19 02:15:42 +0600

    added validation logic

--- src/components/Users/AddUser.jsx
+++ src/components/Users/AddUser.jsx
@@ -1,31 +1,41 @@
-import React,{useState} from "react";
+import React, { useState } from "react";
 import Card from "../UI/Card";
 import Button from "../UI/Button";
 import "./AddUser.css";
 
 const AddUser = () => {
-  const [enteredUsername, setUsername] = useState("")
+  const [enteredUsername, setUsername] = useState("");
   const usernameChangeHandler = (event) => {
-    setUsername(event.target.value)
-  }
-  const [enteredAge, setAge] = useState("")
+    setUsername(event.target.value);
+  };
+  const [enteredAge, setAge] = useState("");
   const ageChangeHandler = (event) => {
-    setAge(event.target.value)
-  }
+    setAge(event.target.value);
+  };
   const addUserHandler = (event) => {
     event.preventDefault();
-    console.log(enteredUsername,enteredAge)
-    setUsername("")
-    setAge("")
+    console.log(enteredUsername, enteredAge);
+    setUsername("");
+    setAge("");
   };
 
   return (
     <Card className="input">
       <form onSubmit={addUserHandler}>
         <label htmlFor="username">Username</label>
-        <input id="username" type="text" onChange={usernameChangeHandler} value={enteredUsername} />
+        <input
+          id="username"
+          type="text"
+          onChange={usernameChangeHandler}
+          value={enteredUsername}
+        />
         <label htmlFor="age">Age</label>
-        <input id="age" type="number" onChange={ageChangeHandler} value={enteredAge} />
+        <input
+          id="age"
+          type="number"
+          onChange={ageChangeHandler}
+          value={enteredAge}
+        />
         <Button type="submit">Add User</Button>
       </form>
     </Card>
