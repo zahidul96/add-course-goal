1d0f24fb0a6809dec6dfee13a597d2d443da985b
zahidul96 <zahidulhussain111@gmail.com>
2026-02-19 13:16:23 +0600

    Added an errorModal

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,23 @@
-import React from "react"
-import AddUser from "./components/Users/AddUser"
-import UsersList from "./components/Users/UsersList"
+import React, { useState } from "react";
+import AddUser from "./components/Users/AddUser";
+import UsersList from "./components/Users/UsersList";
+
 function App() {
-  let users = [{name:"max",age:29}]
+  const [usersList, setUsersList] = useState([]);
+  const addUserHandler = (uName, uAge) => {
+    setUsersList((prevUsersList) => {
+      return [
+        ...prevUsersList,
+        { name: uName, age: uAge, id: Math.random().toString() },
+      ];
+    });
+  };
   return (
     <div>
-        <AddUser/>
-        <UsersList users={users}/>
+      <AddUser onAddUser={addUserHandler} />
+      <UsersList users={usersList} />
     </div>
-      
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/UI/Button.jsx
+++ src/components/UI/Button.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 import "./Button.css";
+
 const Button = (props) => {
   return (
-    <div>
-      <button className="button" type={props.type}>{props.children}</button>
-    </div>
-  )
-}
+    <button
+      className="button"
+      type={props.type || "button"}
+      onClick={props.onClick}
+    >
+      {props.children}
+    </button>
+  );
+};
+
 export default Button;
